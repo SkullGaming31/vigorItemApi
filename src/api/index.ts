0504e92278fce1bb3eac8eac584ea00ae7848fdb
skullgaming31 <canadianfan11@hotmail.com>
2023-05-31 22:17:12 -0230

    /api/v1 now lists all endpoints for the app

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,7 +12,13 @@ const router = express.Router();
 
 router.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: `endpoints: \n NOTE: any :name is to be repleaces with the name of the item you are trying to search for.
+    /weapons, /weapons/:name \n 
+    /consumables, /consumables/:name \n 
+    /tools, /tools/:name \n 
+    /traps, /traps/:name \n
+    /throwables, /throwables/:name \n
+    /melees, /melees/:name \n`,
   });
 });
 
